6592fd5afede410210f4fe1d0d2aec79a7a2f409
ShishirShekhar <sspdav02@gmail.com>
2023-06-06 17:23:48 +0530

    responsiveness

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,12 +2,17 @@ import Button from "../Button";
 
 const Project = ({ project }) => {
   return (
-    <div className="flex flex-col gap-2 border-2 border-black rounded-xl p-2 px-6 w-[600px]">
+    <div className="flex flex-col gap-2 border-2 border-black rounded-xl p-2 px-6 md:w-[600px]">
       <h1 className="text-2xl font-bold">{project.title}</h1>
 
-      <div className="flex gap-2">
+      <div className="flex flex-wrap gap-2">
         {project.tech_stack.map((tech, index) => (
-          <p key={index} className="bg-gray-400 py-2 px-3 text-white text-xs rounded-xl">{tech}</p>
+          <p
+            key={index}
+            className="bg-gray-400 py-2 px-3 text-white text-xs rounded-xl"
+          >
+            {tech}
+          </p>
         ))}
       </div>
 
